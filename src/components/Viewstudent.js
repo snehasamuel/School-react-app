41e762bb1930aa499fced7e00d11fc73f6b54d81
snehasamuel <snehasamuel297@gmail.com>
2022-05-27 06:30:37 +0530

    api integrated

--- src/components/Viewstudent.js
+++ src/components/Viewstudent.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Viewstudent = () => {
-  var viewstudent=[{"admission_number":"4545","rollno":"12","name":"Sneha","classname":"10","parent":"Samuel","phn_no":"8794560133","address":"abc"},
-  {"admission_number":"4542","rollno":"4","name":"Sajan","classname":"12","parent":"Anil","phn_no":"45526561","address":"bjh"},
-  {"admission_number":"54513","rollno":"1","name":"Leana","classname":"8","parent":"Varghese","phn_no":"546113589","address":"gfgdg"},
-  {"admission_number":"7946549","rollno":"9","name":"Merin","classname":"9","parent":"John","phn_no":"545464","address":"habddb"},
-  {"admission_number":"164546","rollno":"2","name":"Cyandra","classname":"7","parent":"Sanjo","phn_no":"7846154","address":"asdgayu"}]
+  var [viewstudent,setViewstud]=useState([])
+  var [load,setLoad]=useState(true)
+ axios.get("http://localhost:5000/api/viewstudent").then((response)=>{
+console.log(response.data)
+setViewstud(response.data)
+setLoad(false)
+ })
   return (
     <div>
       <Header/>
@@ -14,33 +17,41 @@ const Viewstudent = () => {
         <div className='row'>
           <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
 
-          <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Admission Number</th>
-      <th scope="col">Roll Number</th>
-      <th scope="col">Name</th>
-      <th scope="col">Class</th>
-      <th scope="col">Parent's Name</th>
-      <th scope="col">Mobile Number</th>
-      <th scope="col">Address</th>
-    </tr>
-  </thead>
-  <tbody>
-  {viewstudent.map((value,key)=>{
-     return <tr>
-      <th scope={value.admission_number}>1</th>
-      <td>{value.rollno}</td>
-      <td>{value.name}</td>
-      <td>{value.class}</td>
-      <td>{value.parent}</td>
-      <td>{value.phn_no}</td>
-      <td>{value.address}</td>
-    </tr>
-  })}
-    
-  </tbody>
-</table>
+          {load ? 
+          <div className="d-flex justify-content-center">
+          <div className="spinner-border text-success" role="status">
+            <span className="sr-only"></span>
+          </div>
+        </div>:
+          <table className="table table-success">
+          <thead className='table-primary'>
+            <tr>
+              <th scope="col">Admission Number</th>
+              <th scope="col">Roll Number</th>
+              <th scope="col">Name</th>
+              <th scope="col">Class</th>
+              <th scope="col">Parent's Name</th>
+              <th scope="col">Mobile Number</th>
+              <th scope="col">Address</th>
+            </tr>
+          </thead>
+          <tbody>
+          {viewstudent.map((value,key)=>{
+             return <tr>
+              <th scope={value.Admissionno}>1</th>
+              <td>{value.Rollno}</td>
+              <td>{value.Name}</td>
+              <td>{value.Class}</td>
+              <td>{value.Parent}</td>
+              <td>{value.Phone}</td>
+              <td>{value.Address}</td>
+              
+            </tr>
+          })}
+            
+          </tbody>
+        </table>}
+
           </div>
 
         </div>
